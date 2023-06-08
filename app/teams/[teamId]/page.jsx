91cc8b1a75ffe7a1b50c404c1d107a9361eb0300
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-06-08 15:56:49 -0400

    Determine if team is overpaying

--- app/teams/[teamId]/page.jsx
+++ app/teams/[teamId]/page.jsx
@@ -2,13 +2,37 @@ import getAllPlayers from "../../api/getAllPlayers";
 
 export default async function Page({ params }) {
   const data = await getAllPlayers();
+  const marketSalary = "123,600,000";
+
   const teamId = params.teamId;
 
   const roster = data.filter((team) => team.team === teamId);
 
+  const currentSalary = roster.map((player) =>
+    player.currentSalary.replace("$", "")
+  );
+  const salary = currentSalary.map((value) =>
+    Number(value.replaceAll(",", ""))
+  );
+  let total = 0;
+  salary.map((value) => {
+    total = total + value;
+  });
+  console.log(total);
+
   return (
     <div>
       <h1>Team: {teamId}</h1>
+      <h1
+        className={
+          Number(marketSalary.replaceAll(",", "")) < total
+            ? "text-red-500"
+            : "text-green-600"
+        }
+      >
+        ${total}
+      </h1>
+      <h1>NBA Salary Cap: ${marketSalary}</h1>
       {roster.map((player) => {
         return (
           <>
