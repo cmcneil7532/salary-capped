bc0b8609792d2ad56ce2b23044aa86e7eb5dc4d9
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-06-08 11:14:30 -0400

    grab all player contracts

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import Link from "next/link";
+import { JSDOM } from "jsdom";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(
+    "https://www.basketball-reference.com/contracts/players.html"
+  );
+  const html = await response.text();
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+
+  const playerContracts =
+    document.querySelector("#player-contracts").textContent;
+
+  console.log("Player Contract--------------", playerContracts);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Salary Capped</h1>
+    <main className="min-h-full flex flex-col justify-center items-center mt-24">
+      <h1 className="text-3xl">$alary Capped</h1>
+      <p>Track your favorite teams budget</p>
+      <Link href="#teams" className="border-2 border-rose-500 rounded-lg">
+        Track your team
+      </Link>
     </main>
   );
 }
