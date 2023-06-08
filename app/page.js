ab16f5b0d127a5f79aeceefaba08c40d20449faa
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-06-08 12:20:44 -0400

    Get all players data

--- app/page.js
+++ app/page.js
@@ -1,24 +1,36 @@
 import Link from "next/link";
-import { JSDOM } from "jsdom";
+import * as cheerio from "cheerio";
 
 export default async function Home() {
   const response = await fetch(
     "https://www.basketball-reference.com/contracts/players.html"
   );
   const html = await response.text();
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-
-  const playerContracts =
-    document.querySelector("#player-contracts").textContent;
-
-  console.log("Player Contract--------------", playerContracts);
+  const $ = cheerio.load(html);
+  const allRows = $("#player-contracts > tbody > tr");
+  const playerData = [];
+  allRows.each((index, element) => {
+    const tds = $(element).find("td");
+    const name = $(tds[0]).text();
+    const team = $(tds[1]).text();
+    const currentSalary = $(tds[2]).text();
+    const nextYearSalary = $(tds[3]).text();
+    const guranteed = $(tds[8]).text();
+    playerData.push({
+      Name: name,
+      Team: team,
+      currentSalary: currentSalary,
+      "Next years": nextYearSalary,
+      Guranteed: guranteed,
+    });
+  });
+  console.log(playerData);
 
   return (
     <main className="min-h-full flex flex-col justify-center items-center mt-24">
       <h1 className="text-3xl">$alary Capped</h1>
       <p>Track your favorite teams budget</p>
-      <Link href="#teams" className="border-2 border-rose-500 rounded-lg">
+      <Link href="/teams" className="border-2 border-rose-500 rounded-lg">
         Track your team
       </Link>
     </main>
