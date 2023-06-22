157921627b2883a5b3d38fbc8b762f7b0f03a552
Christian McNeil <christianmcneil@christiansmbp2.mynetworksettings.com>
2023-06-22 15:19:50 -0400

    Animate modal

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Teams from "./teams/page";
 import pictures from "../public/pictures";
 export default async function Home() {
   return (
-
     <main>
       <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-black to-gray-900">
         <img className="w-[600px] p-2" src={pictures.logo.src} />
@@ -12,7 +11,7 @@ export default async function Home() {
         </p>
         <Link
           href="#teams"
-          className="font-bold rounded-lg px-4 py-2 border-2 border-gray-200 text-gray-200 hover:bg-gray-200 hover:text-gray-900 duration-300"
+          className="font-bold rounded-lg px-4 py-2 border-2 border-gray-200 text-gray-200 hover:bg-orange-700 hover:text-gray-900 duration-300"
         >
           Get Started
         </Link>
@@ -20,7 +19,9 @@ export default async function Home() {
       <section className="bg-gradient-to-b from-gray-900 to-gray-800">
         <Teams />
       </section>
-      <footer className="bg-slate-800  text-center text-sm text-slate-600">Developed By: Grant Gilman & Christian McNeil | 2023 All Rights Reserved</footer>
+      <footer className="bg-slate-800  text-center text-sm text-slate-600">
+        Developed By: Grant Gilman & Christian McNeil | 2023 All Rights Reserved
+      </footer>
     </main>
   );
 }
